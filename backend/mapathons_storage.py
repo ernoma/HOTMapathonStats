f9a40437bfc6e9044ce68b8cd8cf65cf65a411ad
Erno Mäkinen <ernoma@gmail.com>
2018-02-03 23:55:23 +0200

    Add mapathons_storage.py file, MapathonsStorage class and function declarations for storing and retrieving mapathons data

---
+++ backend/mapathons_storage.py
@@ -0,0 +1,19 @@
+
+class MapathonsStorage(object):
+    """
+    Functionality for storing and retrieving mapathons data including
+    - Changes done during a mapathon
+    - Usernames of those who did the changes during the mapathon
+    - List of all mapathons that have a statistics page
+    """
+
+    def storeMapathon(self, mapathon_data):
+        pass
+
+    def getAllMapathons(self):
+        pass
+
+    def getMapathonByID(self, mapathon_id):
+        pass
+
+    
