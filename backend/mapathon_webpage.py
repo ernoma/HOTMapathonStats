5b02152cd8e9c75c25e81b4105b3f39ab57008de
Erno Mäkinen <ernoma@gmail.com>
2018-02-07 23:46:42 +0200

    Add mapathon_webpage.py, MapathonWebPage class and function declarations for creating and storing the mapathon web page

---
+++ backend/mapathon_webpage.py
@@ -0,0 +1,19 @@
+
+class MapathonWebPage(object):
+    """
+    Functionality for
+    - creating mapathon web page (HTML) and corresponding js and css files
+    - storing the created HTML, js and css files.
+    """
+
+    def create_mapathon_web_page(self, mapathon_id):
+        pass
+        # TODO use the mapathon_id to retrieve mapathon data from the MapathonsStorage defined in the
+        # mapathons_storage.py and create mapathon web page (HTML) and corresponding js and css files
+        # based on the data
+
+    def store_mapathon_web_page(self):
+        pass
+        # TODO store the created HTML, js and css files
+
+
