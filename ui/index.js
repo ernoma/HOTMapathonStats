26cc332169b8eab07a9a28758886bc6cdf82f6f6
Erno Mäkinen <ernoma@gmail.com>
2018-02-28 00:00:02 +0200

    Show count of users in the UI

--- ui/index.js
+++ ui/index.js
@@ -11,12 +11,12 @@ function getMapathons() {
 function handleMapathonsData(data) {
     console.log(data);
     data.forEach(item => {
-        // TODO add information of the users
         var html = '<div class="card"><div class="card-body"><h5 class="card-title">' + item.mapathon_info.mapathon_title + '</h5>' +
             '<p class="card-text">Mapathon held on ' +
             item.mapathon_info.mapathon_date + ' at ' +
             item.mapathon_info.mapathon_time_utc + ' (UTC) for project ' +
-            '<a href="https://tasks.hotosm.org/project/' + item.mapathon_info.project_number + '">' + item.mapathon_info.project_number + '</a>.</p>' +
+            '<a target="_blank" href="https://tasks.hotosm.org/project/' + item.mapathon_info.project_number + '">' + item.mapathon_info.project_number + '</a>. ' +
+            item.mapathon_users.length + ' <i class="fa fa-user" aria-hidden="true"></i></p>' +
             '<a href="#" class="btn btn-primary">Statistics</a></div></div>';
         $("#mapathonList").append(html);
     });
