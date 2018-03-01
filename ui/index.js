c8dd17ef7888f3e318206a707cc26fb0b1760511
Erno Mäkinen <ernoma@gmail.com>
2018-03-01 23:58:04 +0200

    Minor code addition to handle mapathon list button clicks and eventually display the statistics pages

--- ui/index.js
+++ ui/index.js
@@ -17,7 +17,14 @@ function handleMapathonsData(data) {
             item.mapathon_info.mapathon_time_utc + ' (UTC) for project ' +
             '<a target="_blank" href="https://tasks.hotosm.org/project/' + item.mapathon_info.project_number + '">' + item.mapathon_info.project_number + '</a>. ' +
             item.mapathon_users.length + ' <i class="fa fa-user" aria-hidden="true"></i></p>' +
-            '<a href="#" class="btn btn-primary">Statistics</a></div></div>';
+            '<a href="#" id="' + item._id.$oid + '" class="btn btn-primary">Show statistics page</a></div></div>';
         $("#mapathonList").append(html);
+
+        $("#" + item._id.$oid).on('click', showMapathon);
     });
+}
+
+function showMapathon(event) {
+    // TODO use event.target.id to show mapathon statistics page
+    console.log(event);
 }
