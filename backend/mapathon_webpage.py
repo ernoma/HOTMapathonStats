d686c9e063624e0dc8e289931f87a27c32914745
Erno Mäkinen <ernoma@gmail.com>
2018-03-11 00:01:00 +0200

    Read template files in the mapathon_webpage.py

--- backend/mapathon_webpage.py
+++ backend/mapathon_webpage.py
@@ -1,5 +1,6 @@
 
 import os
+from pprint import pprint
 
 class MapathonWebPage(object):
     """
@@ -9,6 +10,7 @@ class MapathonWebPage(object):
     """
 
     STORE_BASE_PATH = "mapathon_stats"
+    MAPATHON_TEMPLATE_BASE_PATH = "templates"
 
     def __init__(self, mapathons_storage):
         self.mapathons_storage = mapathons_storage
@@ -19,15 +21,27 @@ class MapathonWebPage(object):
         # based on the data
         self.mapathon_data = self.mapathons_storage.get_mapathon_by_ID(mapathon_id)
 
+        self.mapathon_base_filename = 'mapathon_' + \
+            self.mapathon_data['mapathon_info']['mapathon_date'] + '_' + \
+            str(self.mapathon_data['mapathon_info']['mapathon_time_utc']) + '_' + \
+            self.mapathon_data['mapathon_info']['project_number']
+
+        with open(os.path.join(MapathonWebPage.MAPATHON_TEMPLATE_BASE_PATH, 'mapathon.html')) as template_html_file:
+            self.template_html = template_html_file.read()
+            pprint(self.template_html)
+        with open(os.path.join(MapathonWebPage.MAPATHON_TEMPLATE_BASE_PATH, 'mapathon.css')) as template_css_file:
+            self.template_css = template_css_file.read()
+            pprint(self.template_css)
+        with open(os.path.join(MapathonWebPage.MAPATHON_TEMPLATE_BASE_PATH, 'mapathon.js')) as template_js_file:
+            self.template_js = template_js_file.read()
+            pprint(self.template_js)
+
     def store_mapathon_web_page(self):
         # TODO store the created HTML, js and css files
         # TODO make name safe
         # TODO use mapathon end_time (optional)
         self.mapathon_base_path = os.path.join(
             MapathonWebPage.STORE_BASE_PATH,
-            'mapathon_' +
-            self.mapathon_data['mapathon_info']['mapathon_date'] + '_' +
-            str(self.mapathon_data['mapathon_info']['mapathon_time_utc']) + '_' +
-            self.mapathon_data['mapathon_info']['project_number'])
+            self.mapathon_base_filename)
 
         print(self.mapathon_base_path)
