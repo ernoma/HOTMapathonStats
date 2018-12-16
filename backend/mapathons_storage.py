2a7fbb00460f1847a7db326910dab929db36e554
Erno Mäkinen <ernoma@gmail.com>
2018-12-16 18:19:30 +0200

    Correct a second bug in storing mapathon changes

--- backend/mapathons_storage.py
+++ backend/mapathons_storage.py
@@ -101,13 +101,11 @@ class MapathonsStorage(object):
             with open(os.path.join(output_dir, 'residential_areas.json'), 'w') as outfile:
                 json.dump(mapathon_changes['landuse_residential'], outfile)
 
-        if "landuse_farmland" in types_of_mapping:
+        if "landuse_any_other" in types_of_mapping:
             with open(os.path.join(output_dir, 'landuse_farmland.json'), 'w') as outfile:
                 json.dump(mapathon_changes['landuse_farmland'], outfile)
-        if "landuse_orchard" in types_of_mapping:
             with open(os.path.join(output_dir, 'landuse_orchard.json'), 'w') as outfile:
                 json.dump(mapathon_changes['landuse_orchard'], outfile)
-        if "landuse_any_other" in types_of_mapping:
             with open(os.path.join(output_dir, 'landuse_any_other.json'), 'w') as outfile:
                 json.dump(mapathon_changes['landuse_any_other'], outfile)
 
